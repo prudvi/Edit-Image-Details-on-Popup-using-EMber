dda8733190899b6d3f06c007a153bd75ccf69534
Prudvi <prudvi2k7@gmail.com>
2014-07-15 09:42:19 +0530

    Application Js for Ember Instance Creation.
    Application Js for Ember Instance Creation.

---
+++ app.js
@@ -0,0 +1,9 @@
+var serverPath = '';
+var foldersList = [];
+var path = 'images';
+
+var imageId = 0;
+var imageList = new Array();
+
+var validExtensions = ['\.jpg', '\.png', '\.gif', '\.jpeg'];
+App = Ember.Application.create();
